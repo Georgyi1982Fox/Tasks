22e46545f92e617475cca5ad0b792e0629f67068
Georgyi Nishni <greserve12@yahoo.com>
2021-01-09 01:44:34 +0300

    task 1 fulfilled

--- src/main/java/tasks/taskone/TaskOne.java
+++ src/main/java/tasks/taskone/TaskOne.java
@@ -1,5 +1,20 @@
 package tasks.taskone;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class TaskOne {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        for(String st: s.split(" ")){
+            System.out.print(new StringBuilder(st).reverse().toString());
+            System.out.print(" ");
+        }
+
+    }
 
 }
